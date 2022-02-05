3263ad59413f3344a27409625fbcf78967516a35
Guilherme Varela <guilhermevarela@protonmail.com>
2022-02-05 19:58:33 +0000

    Add episodic plots for training & validation

--- plots.py
+++ plots.py
@@ -54,6 +54,38 @@ def globally_averaged_plot(mus, img_path):
     plt.savefig(file_name, bbox_inches='tight', pad_inches=0)
     plt.close()
 
+# use this only for continuing tasks.
+# episodes is a series with the episode numbers
+def globally_averaged_plot2(mus, img_path, episodes):
+    
+    globally_averaged_return = np.array(mus)
+    
+    episodes = np.array(episodes)
+    globally_averaged_episodes = []
+    episodes_to_plot = (int(np.min(episodes)), int(np.mean(episodes)), int(np.max(episodes)))
+    for episode in episodes_to_plot: 
+        globally_averaged_episodes.append(globally_averaged_return[episodes == episode])
+    Y = np.vstack(globally_averaged_episodes).T
+
+    n_steps = Y.shape[0]
+    X = np.linspace(1, n_steps, n_steps)
+
+    fig = plt.figure()
+    fig.set_size_inches(FIGURE_X, FIGURE_Y)
+
+
+    labels = [f'episode {epis}' for epis in episodes_to_plot]
+    plt.plot(X, Y, label=labels)
+    plt.xlabel('Time')
+    plt.ylabel('Globally Averaged Return J')
+    plt.legend(loc=4)
+
+    file_name = img_path / 'globally_averaged_return_per_episode.pdf'
+    plt.savefig(file_name, bbox_inches='tight', pad_inches=0)
+    file_name = img_path / 'globally_averaged_return_per_episode.png'
+    plt.savefig(file_name, bbox_inches='tight', pad_inches=0)
+    plt.close()
+
 def cumulative_rewards_plot(rewards, img_path):
     
     fig = plt.figure()
@@ -70,6 +102,49 @@ def cumulative_rewards_plot(rewards, img_path):
     file_name = img_path / 'cumulative_averaged_reward.pdf'
     plt.savefig(file_name, bbox_inches='tight', pad_inches=0)
     file_name = img_path / 'cumulative_averaged_reward.png'
+    plt.savefig(file_name, bbox_inches='tight', pad_inches=0)
+    plt.close()
+
+def cumulative_rewards_plot2(rewards, img_path, episodes, episodic=False):
+    
+    fig = plt.figure()
+    fig.set_size_inches(FIGURE_X, FIGURE_Y)
+
+    rewards = np.array(rewards)
+    episodes = np.array(episodes)
+    rewards_episodes = []
+    if episodic:
+        episodes_to_plot = np.arange(np.max(episodes))
+        for episode in episodes_to_plot: 
+            rewards_episodes.append(np.mean(rewards[episodes == episode]))
+        Y = np.array(rewards_episodes)
+        y_label = 'Average Reward Per Episode'
+        x_label = 'Episodes'
+        labels = 'SARSATabular'
+
+    else:
+        episodes_to_plot = (int(np.min(episodes)), int(np.mean(episodes)), int(np.max(episodes)))
+        for episode in episodes_to_plot: 
+            rewards_episodes.append(rewards[episodes == episode])
+        Y = np.vstack(rewards_episodes).T
+
+        n = np.tile(np.arange(1, Y.shape[0] + 1).reshape((-1, 1)), len(episodes_to_plot))
+
+        Y = np.cumsum(Y, axis=0) / n
+        y_label = 'Cumulative Averages Reward Per Episode'
+        x_label = 'Timesteps'
+        labels = [f'episode {epis}' for epis in episodes_to_plot]
+
+    X = np.linspace(1, Y.shape[0], Y.shape[0])
+
+    plt.plot(X, Y, label=labels)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.legend(loc=4)
+
+    file_name = img_path / 'cumulative_averaged_reward_per_episode.pdf'
+    plt.savefig(file_name, bbox_inches='tight', pad_inches=0)
+    file_name = img_path / 'cumulative_averaged_reward_per_episode.png'
     plt.savefig(file_name, bbox_inches='tight', pad_inches=0)
     plt.close()
 
@@ -145,16 +220,6 @@ def display_Q(env, agent):
     print(f'{margin} GOAL {margin}')
     print(f'GOAL: {goal_pos}')
     print(f'{margin} SARSA {margin}')
-    import timeit
-
-    print(timeit.timeit(stmt='agent.Q', number=1000, globals=locals()))
-    # print(timeit.timeit(stmt='agent.new_Q', number=1000, globals=locals()))
-
-    # np.testing.assert_almost_equal(agent.Q, agent.new_Q)
-
-    print(timeit.timeit(stmt='agent.V', number=1000, globals=locals()))
-    # print(timeit.timeit(stmt='agent.new_V', number=1000, globals=locals()))
-    # np.testing.assert_almost_equal(agent.V, agent.new_V)
 
     action_set = env.action_set
     states_positions_gen = env.next_states()
@@ -239,15 +304,32 @@ def validation_plot(rewards):
 
 if __name__ == '__main__':
     import json
-    from rl import RLAgent
+    # from rl import RLAgent
     from pathlib import Path
     from argparse import Namespace
+    # from operator import itemgetter
+    from agents import get_agent
+    # path = Path('data/20220205155519')
+    # path = Path('data/20220205164323')
+    # SARSA Tabular data/20220205175246 
+    # path = Path('data/20220205175246')
 
-    path = Path('data/20220128152550')
+    # CentralizedActorCritic
+    path = Path('data/20220205171612')
+
+    # snapshot_path = Path(path) / 'snapshot.json'
+    # with snapshot_path.open('r') as f:
+    #     snapshot = json.load(f)
+
+    # globally_averaged_plot2(snapshot['mu'], path, snapshot['episode'])
+    # cumulative_rewards_plot2(d['reward'], path, d['episode'], episodic=True)
     config_path = Path(path) / 'config.json'
-    with config_path.open('r') as f:
-        d = json.load(f)
-    flags = Namespace(**d)
+    with config_path.open('r') as f: flags = json.load(f)
+
+    # change from training to validation
+    flags['max_steps'] = 100
+    flags['episodic'] = False
+    flags = Namespace(**flags)
 
     # Instanciate
     register(
@@ -256,8 +338,39 @@ if __name__ == '__main__':
         kwargs={'flags': flags}
     )
     # TODO: Verify why env != agent.env
+    chkpt_num = max([int(p.parent.stem) for p in path.rglob('*chkpt')])
+    
+    # chkpt_path = sorted(chkpt_nums, key=itemgetter(0))[-1][-1]
     env = gym.make('duo-navigation-v0')
-    agent = RLAgent.load_checkpoint(path, flags.episodes)
+    agent = get_agent(env, flags).load_checkpoint(path, str(chkpt_num))
     agent.env = env
 
-    main(env, agent, path)
+
+    rewards = []
+    for episode in range(100):
+        state = env.reset()
+        agent.reset()
+        actions = agent.act(state)
+
+        episode_rewards = []
+        while True:
+            next_state, next_reward, done, _ = env.step(agent.act(state))
+            
+            episode_rewards.append(np.mean(next_reward))
+            state = next_state
+            if done: break
+        rewards.append(np.cumsum(episode_rewards) / np.arange(1, 101))
+
+    # TODO: LOOP AND ADD LABELS.
+    Y = np.sum(np.stack(rewards), axis=0)
+
+    fig = plt.figure()
+    fig.set_size_inches(FIGURE_X, FIGURE_Y)
+    X = np.linspace(1, Y.shape[0], Y.shape[0])
+    plt.suptitle(f'Validation Round (N=100)')
+    plt.plot(X,Y)
+    plt.xlabel('Timesteps')
+    plt.ylabel('Validation Averaged Rewards.')
+    plt.show()
+
+    # main(env, agent, path)
