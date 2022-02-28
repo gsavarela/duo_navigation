Fix Actor-Critics' TD(0) gradient of theta params.

agents/actor_critic/dsg.py
@@ -95,13 +95,17 @@ class ActorCriticDifferentialSemiGradient(object):
         self.delta = np.clip(self.delta, -1, 1)
         self.mu += self.beta * self.delta
         self.omega += self.alpha * self.delta * get(state)
-        self.theta[cur][:] += self.zeta * self.delta * self.psi(state, cur)
+        self.theta += self.zeta * self.delta * self.psi(state, cur)
         self.step_count += 1
         self.epsilon = float(max(1e-1, self.epsilon - self.epsilon_step))
 
         
     def psi(self, state, action):
-        return (1 - self.PI(state)[action]) * (get(state) / self.tau)
+        res = np.zeros_like(self.theta)
+        for i, x in enumerate(get(state) / self.tau):
+            for j, y in enumerate(self.PI(state)):
+                res[j, i] = (int(action == j) - y)  * x
+        return res
 
     def save_checkpoints(self, chkpt_dir_path, chkpt_num):
         class_name = type(self).__name__.lower()

agents/actor_critic/sg.py
@@ -15,8 +15,6 @@ from functools import lru_cache
 import dill
 import numpy as np
 from numpy.random import rand, choice
-# import torch as th
-# import torch.nn.functional as F
 
 from features import get, label
 from utils import softmax
@@ -31,18 +29,15 @@ class ActorCriticSemiGradient(object):
         self.n_agents = len(env.agents)
         self.n_states = env.n_states
         assert self.n_agents < 3
-        # n_features =  self.n_states // self.n_agents
-        # n_features =  self.n_states + 1
         n_features =  self.n_states
         self.n_joint_actions = len(env.action_set)
         
         # Parameters
         # The feature are state-value function features, i.e,
         # the generalize w.r.t the actions.
+        # LFA
         self.omega = np.zeros(n_features)
         self.theta = np.zeros((len(self.action_set), n_features))
-        # self.theta = th.from_numpy(theta).float()
-        # self.theta.requires_grad = True
 
         # Loop control
         self.step_count = 0
@@ -56,7 +51,7 @@ class ActorCriticSemiGradient(object):
         self.epsilon_step = float(1.2  * (1 - 1e-1) / episodes)
         self.reset(seed=0, first=True)
 
-    def reset(self, seed=0, first=False):
+    def reset(self, seed=None, first=False):
         self.discount = 1.0
 
         if first:
@@ -107,49 +102,26 @@ class ActorCriticSemiGradient(object):
 
         if done:
             self.delta = np.mean(next_rewards) - (get(state) @ self.omega)
+                
         else:
             self.delta = np.mean(next_rewards) + \
-                        ((self.gamma * get(next_state)) - get(state)) @ self.omega
+                    ((self.gamma * get(next_state)) - get(state)) @ self.omega
 
-        self.delta = np.clip(self.delta, -1, 1)
+
+        # Actor update
         self.omega += self.alpha * self.delta * get(state)
 
         # Critic update
-        # psi = self.psi(state, cur)
-        # self.theta[cur] += self.beta *  self.discount * self.delta * self.psi(state, cur)
         self.theta += self.beta *  self.discount * self.delta * self.psi(state, cur)
-
         self.discount *= self.gamma
         self.step_count += 1
 
-        
-    # def psi(self, state, action):
-    #     x = th.tensor(get(state).tolist(), requires_grad=False, dtype=float)
-    #     # x = th.from_numpy(get(state)).float()
-    #     # x.require_grad = False
-    #     print(f'self.theta={self.theta}')
-
-    #     theta = th.tensor(self.theta.tolist(), requires_grad=True, dtype=float)
-
-    #     print(f'theta={theta}')
-    #     y = th.matmul(theta, x)
-    #     z = F.log_softmax(y, dim=-1)
-    #     z.backward(th.ones_like(y))
-
-    #     print(f'theta={theta}')
-    #     print(f'theta.grad={theta.grad}')
-    #     import ipdb; ipdb.set_trace()
-    #     theta_grad_a = theta.grad[action].detach().numpy()
-    #     return theta_grad_a
-
-
     def psi(self, state, action):
-        res = [(int(action == _action) - _prob) * (get(state) / self.tau)
-                for _action, _prob in enumerate(self.PI(state))]
-        return np.vstack(res)
-
-    # def psi(self, state, action):
-    #     return (1 - self.PI(state)[action]) * (get(state) / self.tau)
+        res = np.zeros_like(self.theta)
+        for i, x in enumerate(get(state) / self.tau):
+            for j, y in enumerate(self.PI(state)):
+                res[j, i] = (int(action == j) - y)  * x
+        return res
 
     def save_checkpoints(self, chkpt_dir_path, chkpt_num):
         class_name = type(self).__name__.lower()