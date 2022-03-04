82931f134bf8980f23e2a02a28e3b7f8426e7bd4
Guilherme Varela <guilhermevarela@protonmail.com>
2022-03-04 18:10:19 +0000

    Add snapshot_paths: saves the most frequent paths

--- logs.py
+++ logs.py
@@ -1,8 +1,12 @@
-from pathlib import Path
 from collections import Counter
+import json
+from pathlib import Path
+from operator import itemgetter
 
 import numpy as np
 import pandas as pd
+
+from utils import state2pos
 
 def snapshot_log(episode, env, agent, tr, log_dict, debug=True):
     # Read step variables
@@ -87,4 +91,50 @@ def snapshot_state_actions_log(log, log_dir=None):
         df.to_csv(sa_path)
     return df
 
+
+def snapshot_paths(log_dir, log=None, max_k_paths=10):
+    '''Builds a transiton counter'''
+    log_path = log_dir / 'snapshot.json'
+    if log is None:
+        with log_path.open('r') as f:
+            log = json.load(f)
+    paths = []
+    cur = -1
+    first = True
+    for episode, state in zip(log['episode'], log['state']):
+        if episode != cur:
+            if not first: paths.append(tuple(episode_path))
+            episode_path = []
+            first = False
+        episode_path.append(state)
+        cur = episode
+    paths.append(tuple(episode_path))
+
+    paths_count = Counter(paths).items() 
+    paths_count = sorted(paths_count, key=itemgetter(1), reverse=True)
+    paths_count = paths_count[:max_k_paths]
+    paths, count = zip(*paths_count)
+    df = pd.DataFrame.from_records(paths, index=count)
+
+    # Format state into positions
+    def fn(x): return [str(tup) for tup in map(tuple, state2pos(x))]
+    def gn(x): return np.nan if np.isnan(x) else ','.join(fn(x))
+    df = df.applymap(gn)
+
+    paths_path = Path(log_dir) / 'paths.csv'
+    if log_dir is not None:
+        df.to_csv(paths_path.as_posix())
+    return df
+
+if __name__ == '__main__':
+    # log_dir = Path('data/AC-UNIFORM/08_DEBUG_X1R0/')
+    # paths = ['data/AC-UNIFORM/01_1M_X0R1',
+    # 'data/AC-UNIFORM/02_1M_X0R0', 'data/AC-UNIFORM/03_1M_X1R0',
+    # 'data/AC-UNIFORM/04_1M_X1R1', 'data/AC-UNIFORM/05_1M_X1R1_small',
+    # 'data/AC-UNIFORM/06_5M_X1R1_decay']
+    # for path in paths:
+    #     log_dir = Path(path)
+    #     df = snapshot_paths(log_dir)
+    #     print(df)
+    # pass
 
